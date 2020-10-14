05369937a446c6a2e55091d38248b83beb13167b
Francis Bulu <FrancisB@cursisten.intecbrussel.intra>
2020-10-14 14:43:44 +0200

    Add project files.

---
+++ Array2 - 10getallen/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array2___10getallen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Geef 10 getalen in");
+            int[] getal = new int[10];
+
+            for (int i = 0; i < 10; i++)
+            {
+                getal[i] = Int32.Parse(Console.ReadLine());
+                Console.WriteLine($"Jouw getal nummer {i+1} is: {getal[i]}");
+            }
+
+        }
+    }
+}
